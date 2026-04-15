4c30dcd85a99dd1ef766321b68b7e3740649b3d1
YoussefGhollamallah <youssef.gh@live.fr>
2026-04-15 08:34:19 +0200

    création de l'entité catégorie et mise en place des contraintes

---
+++ src/Entity/Category.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[UniqueEntity(fields: ['name'])]
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 2,
+        max: 100,
+        minMessage: 'Le nom de la catégorie doit comporter au moins {{ limit }} caractères.',
+        maxMessage: 'Le nom de la catégorie ne peut pas dépasser {{ limit }} caractères.',
+    )]
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
